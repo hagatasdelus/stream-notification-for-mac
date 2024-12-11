32fc56df58f7276f9209899d0fd81d0bfff8f3c4
HaGaTa_ <gsiyakav.c08nk@gmail.com>
2024-12-11 23:05:11 +0900

    update doc

--- src/stream_status.py
+++ src/stream_status.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+"""
+This module provides an enum class for stream status.
+"""
+
 __author__ = "Hagata"
 __version__ = "0.0.1"
 __date__ = "2024/12/08 (Created: 2024/10/20)"
