e531ff5295fd33afade8323c8e1050de3fed8aeb
HaGaTa_ <gsiyakav.c08nk@gmail.com>
2024-12-28 23:42:53 +0900

    twitch refactor and add LICENSE

--- src/twitch/__init__.py
+++ src/twitch/__init__.py
@@ -4,9 +4,8 @@ __author__ = "Hagata"
 __version__ = "0.0.1"
 __date__ = "2024/12/08 (Created: 2024/10/20)"
 
-from .twitch import TwitchAPI, TwitchAPIError
+from .twitch import TwitchAPI
 
 __all__ = [
     "TwitchAPI",
-    "TwitchAPIError",
 ]
