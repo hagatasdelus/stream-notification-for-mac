0dc7aa24bf4ab6ea8523d19c89231bc943b3ace2
HaGaTa_ <gsiyakav.c08nk@gmail.com>
2024-12-30 20:53:33 +0900

    terminal doc update

--- src/terminal.py
+++ src/terminal.py
@@ -5,6 +5,10 @@ terminal.py
 This module provides a class for interacting with the terminal.
 """
 
+__author__ = "Hagata"
+__version__ = "0.0.1"
+__date__ = "2024/12/29 (Created: 2024/12/29)"
+
 import asyncio
 import subprocess
 import traceback
@@ -15,10 +19,23 @@ from src.utils import get_logger
 logger = get_logger(__name__)
 
 class Terminal(object):
+    """Class for interacting with the terminal.
+
+    Attributes:
+        base_dir (Path): The base directory of the application.
+    """
     def __init__(self, base_dir: Path) -> None:
+        """Initialize the terminal class.
+        """
         self.base_dir = base_dir
 
     async def launch_terminal(self) -> None:
+        """Launch a new terminal window.
+
+        Raises:
+            FileNotFoundError: If the applescript file is not found.
+            SubprocessError: If an error occurs while executing the applescript.
+        """
         try:
             script_path = Path(self.base_dir, "applescript", "launch_terminal.applescript")
         except FileNotFoundError:
@@ -39,6 +56,12 @@ class Terminal(object):
             return
 
     async def close_terminal(self) -> None:
+        """Close the current terminal window.
+
+        Raises:
+            FileNotFoundError: If the applescript file is not found.
+            SubprocessError: If an error occurs while executing the applescript.
+        """
         try:
             script_path = Path("applescript", "close_terminal.applescript")
         except FileNotFoundError:
